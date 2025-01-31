4a18133b259329d12742fc45b654047e64c4d3ac
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-01-31 16:08:52 +0600

    reder the products

--- script.js
+++ script.js
@@ -9,14 +9,22 @@ async function fetchListOfProducts() {
     const data = await response.json();
 
     if (data?.products?.length) renderProducts(data.products);
-   
   } catch (error) {
     console.log(" Error fetching products:", error);
   }
 }
 
 function renderProducts(products) {
-  console.log(products);
+  productsContainer.innerHTML = products.map(
+    (product) => `
+     <div class="product">
+       <img src="${product.thumbnail}" alt="${product.title}" width="100">
+        <h3>${product.title}</h3>
+        <p>${product.description}</p>
+        <p>Price: $${product.price}</p>
+      </div>
+  `
+  ).join(" ");
 }
 
 fetchListOfProducts();
